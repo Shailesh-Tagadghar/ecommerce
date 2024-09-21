3ee19ab73c7a8a9602f210805af120fb668ca741
shailesh_tagadghar <shaileshtagadghar13@gmail.com>
2024-09-21 12:50:44 +0530

    .

--- lib/Modules/Auth/services/api_service.dart
+++ lib/Modules/Auth/services/api_service.dart
@@ -6,14 +6,9 @@ import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
 
 class ApiService {
-  // static const String baseUrl = 'http://fashionapp.idealake.com/api';
-  // static const String imageBaseUrl =
-  //     'https://fashionapp.idealake.com/'; // Base URL for images
-
   // Register a user
   static Future<void> registerUser(
       Map<String, dynamic> registrationData) async {
-    // final url = Uri.parse('$baseUrl/${ApiConstants.register}');
     final url = Uri.parse('${ApiConstants.baseUrl}${ApiConstants.register}');
 
     final storage = GetStorage();
@@ -61,8 +56,6 @@ class ApiService {
         if (response.statusCode == 200) {
           final data = jsonDecode(value);
           print('User registered successfully');
-          // Store user data in GetStorage
-          // final storage = GetStorage();
           GetStorage().write('user_data', data);
           print('Data after registration : $data');
         } else {
@@ -77,8 +70,6 @@ class ApiService {
   }
 
   static Future<void> loginUser(Map<String, dynamic> loginData) async {
-    // final url = Uri.parse(
-    //     '$baseUrl/${ApiConstants.login}'); // Adjust the endpoint if needed
     final url = Uri.parse('${ApiConstants.baseUrl}${ApiConstants.login}');
 
     final fcmToken = GetStorage().read('fcm_token');
@@ -121,10 +112,7 @@ class ApiService {
   // Method to change password
   static Future<void> changePassword(
       Map<String, dynamic> data, String token) async {
-    // final String url =
-    //     '$baseUrl/${ApiConstants.forgetPassword}'; // Update with your actual API URL
-    final String url =
-        '${ApiConstants.baseUrl}${ApiConstants.forgetPassword}';
+    const String url = '${ApiConstants.baseUrl}${ApiConstants.forgetPassword}';
 
     final response = await http.post(
       Uri.parse(url),
