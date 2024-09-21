06b0fadbaa2f54e15bc4292b601abac5fd39dacd
shailesh_tagadghar <shaileshtagadghar13@gmail.com>
2024-09-21 17:22:55 +0530

    added snackbar to the code

--- lib/Modules/Auth/services/api_service.dart
+++ lib/Modules/Auth/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:ecommerce/Utils/Constants/api_constants.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
@@ -56,15 +58,28 @@ class ApiService {
         if (response.statusCode == 200) {
           final data = jsonDecode(value);
           print('User registered successfully');
+          Get.snackbar("Success", "User registered successfully",
+              snackPosition: SnackPosition.BOTTOM,
+              backgroundColor: Colors.green,
+              colorText: Colors.white);
           GetStorage().write('user_data', data);
           print('Data after registration : $data');
         } else {
           print('Server returned an error: ${response.statusCode}');
+          Get.snackbar(
+              "Error", "Failed to register user: ${response.statusCode}",
+              snackPosition: SnackPosition.BOTTOM,
+              backgroundColor: Colors.red,
+              colorText: Colors.white);
           throw Exception('Failed to register user');
         }
       });
     } catch (e) {
       print('Error during registration: $e');
+      Get.snackbar("Error", "Error during registration: $e",
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red,
+          colorText: Colors.white);
       throw Exception('Failed to register user');
     }
   }
@@ -93,7 +108,10 @@ class ApiService {
 
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
-
+        Get.snackbar("Success", "User logged in successfully",
+            snackPosition: SnackPosition.BOTTOM,
+            backgroundColor: Colors.green,
+            colorText: Colors.white);
         print('User logged in successfully');
         // Store the user information in GetStorage for persistence
         final storage = GetStorage();
@@ -101,10 +119,18 @@ class ApiService {
         print('Data after Login : $data');
       } else {
         print('Server returned an error: ${response.statusCode}');
+        Get.snackbar("Error", "Failed to log in: ${response.statusCode}",
+            snackPosition: SnackPosition.BOTTOM,
+            backgroundColor: Colors.red,
+            colorText: Colors.white);
         throw Exception('Failed to log in');
       }
     } catch (e) {
       print('Error during login: $e');
+      Get.snackbar("Error", "Error during login: $e",
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red,
+          colorText: Colors.white);
       throw Exception('Failed to log in');
     }
   }
@@ -113,22 +139,36 @@ class ApiService {
   static Future<void> changePassword(
       Map<String, dynamic> data, String token) async {
     const String url = '${ApiConstants.baseUrl}${ApiConstants.forgetPassword}';
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Authorization': 'Bearer $token', // Add the token in the header
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(data), // Encode the request body to JSON
+      );
 
-    final response = await http.post(
-      Uri.parse(url),
-      headers: {
-        'Authorization': 'Bearer $token', // Add the token in the header
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode(data), // Encode the request body to JSON
-    );
-
-    if (response.statusCode == 200) {
-      // Password change successful
-      print('Password changed successfully');
-    } else {
-      // Handle failure
-      throw Exception('Failed to change password: ${response.body}');
+      if (response.statusCode == 200) {
+        // Password change successful
+        Get.snackbar("Success", "Password changed successfully",
+            snackPosition: SnackPosition.BOTTOM,
+            backgroundColor: Colors.green,
+            colorText: Colors.white);
+        print('Password changed successfully');
+      } else {
+        // Handle failure
+        Get.snackbar("Error", "Failed to change password: ${response.body}",
+            snackPosition: SnackPosition.BOTTOM,
+            backgroundColor: Colors.red,
+            colorText: Colors.white);
+        throw Exception('Failed to change password: ${response.body}');
+      }
+    } catch (e) {
+      Get.snackbar("Error", "Error changing password: $e",
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red,
+          colorText: Colors.white);
     }
   }
 
@@ -161,10 +201,18 @@ class ApiService {
         // Check if data is a Map and contains the 'coupons' key
         if (data is Map && data.containsKey('data')) {
           List<dynamic> coupons = data['data'];
+          // Get.snackbar("Success", "Coupons fetched successfully",
+          //     snackPosition: SnackPosition.BOTTOM,
+          //     backgroundColor: Colors.green,
+          //     colorText: Colors.white);
           return coupons
               .map((coupon) => coupon as Map<String, dynamic>)
               .toList();
         } else {
+          Get.snackbar("Error", "Unexpected response format",
+              snackPosition: SnackPosition.BOTTOM,
+              backgroundColor: Colors.red,
+              colorText: Colors.white);
           throw Exception('Unexpected response format');
         }
       } else {
@@ -173,6 +221,10 @@ class ApiService {
       }
     } catch (e) {
       print('Error fetching coupons: $e');
+      Get.snackbar("Error", "Failed to fetch coupons: $e",
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red,
+          colorText: Colors.white);
       throw Exception('Failed to fetch coupons');
     }
   }
